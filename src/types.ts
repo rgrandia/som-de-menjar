23d4aa7f5dd72721a0b74e9d88633118b645d0ad
Grandia <167295706+rgrandia@users.noreply.github.com>
2026-04-08 21:39:19 +0200

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -25,23 +25,25 @@ export interface Restaurant {
   data_afegit: string
 }
 
-export interface RestaurantCreate {
+export interface Restaurant {
+  id?: string
   nom: string
   adreca?: string
   barri?: string
   ciutat?: string
   tipus_cuina?: string
-  preu?: string
-  puntuacio?: number
+  rang_preu?: string
+  puntuacio_global?: number
   puntuacio_menjar?: number
   puntuacio_servei?: number
   puntuacio_ambient?: number
   telefon?: string
   web?: string
-  maps_url?: string
+  google_maps_url?: string
   afegit_per?: string
   notes?: string
   visitat?: boolean
+  created_at?: string
 }
 
 export interface Filtres {
